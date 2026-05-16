e79283734cdff54ce174bfc3220b85c20bc46052
Ian Kincaid <i.s.kincaid@gmail.com>
2026-05-15 20:06:05 -0400

    added simple service functions for plants

--- src/main/java/com/garden/dop/service/PlantService.java
+++ src/main/java/com/garden/dop/service/PlantService.java
@@ -2,6 +2,8 @@ package com.garden.dop.service;
 
 import com.garden.dop.data.Plant;
 
+import java.time.LocalDate;
+
 public class PlantService {
 
     // get all plants
@@ -11,6 +13,43 @@ public class PlantService {
     // get all plants in a bed
 
     // create a plant
+    public Plant createPlant(Plant plant){
+        return new Plant(
+                plant.id(),
+                plant.name(),
+                plant.genus(),
+                plant.species(),
+                plant.spreadRadius(),
+                plant.currentRadius(),
+                plant.datePlanted(),
+                plant.dateWatered(),
+                plant.daysDryDown(),
+                plant.daysToHarvest(),
+                plant.hardinessZone(),
+                plant.lifeSpan(),
+                plant.lowLight()
+        );
+    }
+
+    // water a plant
+    // will move someone else later
+    public Plant waterPlant(Plant plant){
+        return new Plant(
+                plant.id(),
+                plant.name(),
+                plant.genus(),
+                plant.species(),
+                plant.spreadRadius(),
+                plant.currentRadius(),
+                plant.datePlanted(),
+                LocalDate.now(),
+                plant.daysDryDown(),
+                plant.daysToHarvest(),
+                plant.hardinessZone(),
+                plant.lifeSpan(),
+                plant.lowLight()
+        );
+    }
 
     // update a plant
 
